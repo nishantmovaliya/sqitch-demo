a12665c6884e433074d69763ac742207209bdaf2
Nishant Movaliya <nishant.movaliya@scaletech.xyz>
2023-11-03 15:06:33 +0530

    Add `insert_user()` and `change_pass()`.

---
+++ revert/change_pass.sql
@@ -0,0 +1,4 @@
+-- Revert flipr:change_pass from pg
+BEGIN;
+DROP FUNCTION flipr.change_pass(TEXT, TEXT, TEXT);
+COMMIT;
---
+++ verify/insert_user.sql
@@ -0,0 +1,5 @@
+-- Verify flipr:insert_user on pg
+
+BEGIN;
+SELECT has_function_privilege('flipr.insert_user(text, text)', 'execute');
+ROLLBACK;
